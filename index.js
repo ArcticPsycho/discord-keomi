44fc4e95d138a986270e765164d20f3bff62e9df
ArcticPsycho <73002346+ArcticPsycho@users.noreply.github.com>
2020-10-18 17:44:06 -0300

    Discord bot - Keomi
    Discord bot currently being developed.

---
+++ index.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+const client = new Discord.Client();
+const prefix = "!";
+
+client.on("message", function(message) {
+if (message.author.bot) return;
+if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+const args = message.content.slice(prefix.length).trim().split(' ');
+const command = args.shift().toLowerCase();
+const commandBody = message.content.slice(prefix.length);
+
+if (command === "ping") {
+const timeTaken = Date.now() - message.createdTimestamp;
+message.channel.send(`Pong! This message had a latency of ${timeTaken}ms.`);
+} else if (message.content === `${prefix}beep`) {
+		message.channel.send('Boop!');
+	} else if (message.content === `${prefix}server`) {
+		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+	} else if (message.content === `${prefix}user info`)  {
+		message.channel.send(`***Username:***  ${message.author.tag}\n***User ID:***  ${message.author.id}\n***Is this user a bot?:***  ${message.author.bot}\n***User created at:***  ${message.author.createdAt}`);
+	} else if (command === 'args-info') {
+	if (!args.length) {
+		return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+	}
+
+	message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+}
+});
+client.login(config.BOT_TOKEN);
