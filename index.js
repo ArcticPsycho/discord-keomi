1f2d8b4b574cc9edc72727e5a51ad18ebf1b4f3a
ArcticPsycho <73002346+ArcticPsycho@users.noreply.github.com>
2020-10-21 11:05:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
 const client = new Discord.Client();
-const prefix = "!";
+const prefix = "k!";
 
 client.on("message", function(message) {
 if (message.author.bot) return;
