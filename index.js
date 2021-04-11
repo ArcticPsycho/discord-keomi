2a592c2caa27174a5d3148f9dffe430acb87db96
0Page <68745533+0Page@users.noreply.github.com>
2021-04-12 00:05:47 +0200

    Changed all the if else's to a switch conditional
    I dunno why, it's 1am and I am bored
    
    Also I don't know javascript, but I am curious to find out if what Ive written even works, so uh PLEASE KEEP THE ORIGINAL COPY OF THE CODE IN CASE THIS ONE DOESNT WORK.
    
    Also I couldn't figure out what "args-info" command was supposed to do, so it has been left out. That and the help command were broken due to some weirdness happening going forward from line 54 of the old code, no idea what.
    
    Rye#1323 on Discord

--- index.js
+++ index.js
@@ -4,29 +4,42 @@ const client = new Discord.Client();
 const prefix = "k!";
 
 client.on("message", function(message) {
-if (message.author.bot) return;
-if (!message.content.startsWith(prefix) || message.author.bot) return;
+	
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-const args = message.content.slice(prefix.length).trim().split(' ');
-const command = args.shift().toLowerCase();
-const commandBody = message.content.slice(prefix.length);
+	const args = message.content.slice(prefix.length).trim().split(' ');
+	const command = args.shift().toLowerCase();
+	const commandBody = message.content.slice(prefix.length);
 
-if (command === "ping") {
-const timeTaken = Date.now() - message.createdTimestamp;
-message.channel.send(`Pong! This message had a latency of ${timeTaken}ms.`);
-} else if (message.content === `${prefix}beep`) {
-		message.channel.send('Boop!');
-	} else if (message.content === `${prefix}server`) {
-		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-	} else if (message.content === `${prefix}user info`)  {
-		message.channel.send(`***Username:***  ${message.author.tag}\n***User ID:***  ${message.author.id}\n***Is this user a bot?:***  ${message.author.bot}\n***User created at:***  ${message.author.createdAt}`);
-	} else if (command === 'args-info') {
-	if (!args.length) {
-		return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-	message.channel.send(`Command name: ${command}\nArguments: ${args}`);
-} else if (message.content === `${prefix}help`) {
-                message.channel.send(`Hello! If you're having bot issues, please submit a ticket in the Keomi Support server, which can be found here: https://discord.gg/tctgKsq\nFor command help, please use k!commands!`) {   
+	//Yo, I know no one is working on this anymore, but fuck it, I'll do sum
+	//Gon replace all the command stuff with a Switch for simplicity's sake
+	switch(command) {
 
-}
+		case "ping": //Sends back latency
+			const timeTaken = Date.now() - message.createdTimestamp;
+			message.channel.send(`Pong! This message had a latency of ${timeTaken}ms.`);
+			break;
+
+		case "beep": //sends boop!
+			message.channel.send('Boop!');
+			break;
+
+		case "server": //gets server name and member count
+			message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+			break;
+
+		case "user info": //Information about the user
+			message.channel.send(`***Username:***  ${message.author.tag}\n***User ID:***  ${message.author.id}\n***Is this user a bot?:***  ${message.author.bot}\n***User created at:***  ${message.author.createdAt}`);
+			break;
+
+		case "help":
+			message.channel.send(`Hello! If you're having bot issues, please submit a ticket in the Keomi Support server, which can be found here: https://discord.gg/tctgKsq\nFor command help, please use k!commands!`);
+			break;
+
+		default:
+			message.channel.send('Unknown command!');
+	}
+
 });
 client.login(config.BOT_TOKEN);
